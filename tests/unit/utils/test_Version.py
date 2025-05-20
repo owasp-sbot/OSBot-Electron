279b31f9c911da8520d167e4bbe44eeed93b5df1
Dinis Cruz <dinis.cruz@owasp.org>
2025-05-20 13:00:12 +0100

    first commit

---
+++ tests/unit/utils/test_Version.py
@@ -0,0 +1,22 @@
+import osbot_electron
+from unittest                      import TestCase
+from osbot_utils.utils.Files       import parent_folder, file_name
+from osbot_electron.utils.Version  import Version, version__osbot_electron
+
+
+class test_Version(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.version = Version()
+
+    def test_path_code_root(self):
+        assert self.version.path_code_root() == osbot_electron.path
+
+    def test_path_version_file(self):
+        with self.version as _:
+            assert parent_folder(_.path_version_file()) == osbot_electron.path
+            assert file_name    (_.path_version_file()) == 'version'
+
+    def test_value(self):
+        assert self.version.value() == version__osbot_electron
